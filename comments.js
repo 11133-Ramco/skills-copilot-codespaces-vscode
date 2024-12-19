5d8c28940214038983302efb7c7ea92bad370e11
11133-Ramco <11133@ramco.com>
2024-12-19 05:13:20 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,42 +1,33 @@
 // create web server
-const express = require('express');
-const app = express();
-const port = 3000;
-// create path
-const path = require('path');
+var express = require('express');
+var app = express();
 
-// create comments
-let comments = [
-    {
-        name: 'John',
-        comment: 'Hello'
-    },
-    {
-        name: 'Jane',
-        comment: 'Hi'
-    }
-];
-
-// create route
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+// create a comment
+app.post('/comments', function(req, res) {
+  res.status(201).json({id: 1, message: 'Comment created'});
 });
 
-// create route
-app.get('/comments', (req, res) => {
-    res.json(comments);
+// get all comments
+app.get('/comments', function(req, res) {
+  res.json([{id: 1, message: 'First comment'}]);
 });
 
-// create route
-app.post('/comments', (req, res) => {
-    comments.push({
-        name: req.query.name,
-        comment: req.query.comment
-    });
-    res.json(comments);
+// get a comment by id
+app.get('/comments/:id', function(req, res) {
+  res.json({id: 1, message: 'First comment'});
 });
 
-// create server
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+// update a comment
+app.put('/comments/:id', function(req, res) {
+  res.json({id: 1, message: 'Comment updated'});
+});
+
+// delete a comment
+app.delete('/comments/:id', function(req, res) {
+  res.status(204).send();
+});
+
+// start the server
+app.listen(3000, function() {
+  console.log('Server is running on http://localhost:3000');
 });
